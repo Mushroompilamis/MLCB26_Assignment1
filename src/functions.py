b6afd77f30cddc772cf6a5165117efca12ebbfa1
Megumiiii <megumifushiguro1414@gmail.com>
2026-03-30 11:33:16 +0300

    Starting Task 4

--- src/functions.py
+++ src/functions.py
@@ -8,8 +8,10 @@ from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error,mean_absolute_error,r2_score
 from scipy.stats import pearsonr
+from scipy.stats import spearmanr
 from sklearn.linear_model import ElasticNet,BayesianRidge
 from sklearn.svm import SVR
+from mrmr import mrmr_regression
 
 ###########TASK 1###########
 # 1.2 Preprocessing
@@ -57,7 +59,7 @@ def  preprocessor_pipeline(cpg,metadata):
     transformers = []
     if len(cpg)>0:
         cpq_pipeline=Pipeline([
-            ("imputer", SimpleImputer(strategy="mean")),
+            ("imputer", SimpleImputer(strategy="median")),
             ("scaler", StandardScaler())
         ])
         transformers.append(("cpg",cpq_pipeline,cpg))
@@ -308,7 +310,205 @@ def train_vs_val_all_models(models, X_train, y_train, X_val, y_val):
 
 ###########TASK 3###########
 #3.1  Stability Selection
+def stability_selection(X_train,resamples=50,samples_frac=0.80,top_k=200,seed=42):
+    cpg_cols=[j for j in X_train.columns if j.startswith("cg")]
+    counts=pd.Series(0, index=cpg_cols,dtype=int)
+    for i in range(resamples):
+        sub=X_train.sample(frac=samples_frac,replace=False,random_state=seed + i)
+        #CpG within each subsample
+        imputer=SimpleImputer(strategy="mean")
+        sub_cpg=pd.DataFrame(imputer.fit_transform(sub[cpg_cols]),columns=cpg_cols,index=sub.index)
+        sub_age=sub["age"]
 
+        correlations={}
+        for j in cpg_cols:
+            corr, _ = spearmanr(sub_cpg[j], sub_age)
+            correlations[j] = abs(corr)
+        top_features=sorted(correlations, key=correlations.get, reverse=True)[:top_k]
+        counts[top_features]+=1
+
+        if (i+1) % 10 == 0:
+            print(f"Resample regeneration: {i+1}/{resamples} completed")
+    stable=counts[counts > (resamples/2)]
+    print(f"The stable features are: {len(stable)}")
+    return stable.sort_values(ascending=False),counts.sort_values(ascending=False)
+
+def frequencyplot(counts, path="../figures/stability_selection.png"):
+    plt.figure(figsize=(8, 5))
+    plt.hist(counts.values, bins=20, edgecolor="black")
+    plt.axvline(25, color="red", linestyle="--", label="threshold > 25")
+    plt.xlabel("Selection  Frequency (out of 50 resamples) ")
+    plt.ylabel("Number of CpGs")
+    plt.title("Selection-frequency distribution across CpG features")
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(path, dpi=150, bbox_inches="tight")
+    plt.show()
+    plt.close()
+
+
+#3.2   Minimum Redundancy Maximum Relevance  (mRMR)
+def choosing_k_value(X_train, X_val, k_values=None, path="../figures/mrmr_k_selection.png"):
+    if k_values is None:
+        k_values = [40,60,80,100,120,150,180,200]
+
+    cpg_cols = [i for i in X_train.columns if i.startswith("cg")]
+    results = []
+
+    for k in k_values:
+        print(f"Testing k: {k}")
+        mrmr_features = mrmr_regression(
+            X=X_train[cpg_cols],
+            y=X_train["age"],
+            K=k
+        )
+        # preprocess CpGs
+        preprocessor = preprocessor_pipeline(cpg=mrmr_features, metadata=[])
+        X_train_processed = preprocessor.fit_transform(X_train[mrmr_features])
+        X_val_processed = preprocessor.transform(X_val[mrmr_features])
+
+        # model of choice BR
+        model = BayesianRidge()
+        model.fit(X_train_processed, X_train["age"])
+        y_val_predict = model.predict(X_val_processed)
+        rmse = mean_squared_error(X_val["age"], y_val_predict) ** 0.5
+        results.append({
+            "K": k,
+            "Validation RMSE": rmse
+        })
+        print(f"K: {k} with Validation RMSE: {rmse:.4f}")
+
+    #dataframe of results
+    results = pd.DataFrame(results)
+    b_rmse = results["Validation RMSE"].min()
+    b_candidates = results[results["Validation RMSE"] == b_rmse]
+    b_k = b_candidates["K"].min()
+    print(f"\nThe best chosen K is: {b_k} with Validation RMSE: {b_rmse:.4f}")
+
+    # Plot k vs RMSE
+    plt.figure(figsize=(7, 4))
+    plt.plot(results["K"], results["Validation RMSE"], marker="o")
+    plt.xlabel("K")
+    plt.ylabel("Validation RMSE")
+    plt.title("mRMR: Selection of optimal K")
+    plt.tight_layout()
+    plt.savefig(path, dpi=150, bbox_inches="tight")
+    plt.show()
+    plt.close()
+    return b_k, results
+
+
+def mrmr_function(X_train, k):
+    cpg_cols = [i for i in X_train.columns if i.startswith("cg")]
+    imputer=SimpleImputer(strategy="median")
+    X_train_cpg =pd.DataFrame(imputer.fit_transform(X_train[cpg_cols]),columns=cpg_cols,index=X_train.index)
+    y_train = X_train["age"]
+
+    mrmr_features = mrmr_regression(X=X_train_cpg, y=y_train, K=k)
+    print(f"Selected mRMR {k} features.")
+
+    top10_sfeatures = []
+    for rank, feat in enumerate(mrmr_features[:10], start=1):
+        corr, _ = spearmanr(X_train_cpg[feat], X_train["age"])
+        top10_sfeatures.append({
+            "Rank": rank,
+            "Feature": feat,
+            "Proxy importance (Spearman r)": abs(corr)
+        })
+    top10 = pd.DataFrame(top10_sfeatures)
+    print("\nTop 10 selected features:")
+    print(top10.to_string(index=False))
+    return mrmr_features, top10
+
+
+def overlapping_plot(stability_features, mrmr_features, path="../figures/feature_overlap.png"):
+    set_stability = set(stability_features)
+    set_mrmr = set(mrmr_features)
+    stability = set_stability - set_mrmr
+    overlap = set_stability & set_mrmr
+    mrmr = set_mrmr - set_stability
+
+    print("Stability: ", len(stability))
+    print("Overlap: ", len(overlap))
+    print("mRMR: ", len(mrmr))
+
+    overlap_df = pd.DataFrame({
+        "Group": ["Stability", "Overlap", "mRMR results"],
+        "Count": [len(stability), len(overlap), len(mrmr)]
+    })
+
+    #####################
+    plt.figure(figsize=(6, 5))
+    plt.bar(overlap_df["Group"], overlap_df["Count"], edgecolor="black")
+    plt.title("Overlap between the stability-selected and mRMR-selected feature sets")
+    plt.ylabel("Number of Features")
+    plt.xlabel("Feature Group")
+    plt.tight_layout()
+    plt.savefig(path, dpi=250, bbox_inches="tight")
+    plt.show()
+    plt.close()
+    #####################
+
+    return overlap_df
+
+#3.3  Method comparison and selection
+def feature_comparison(X_train,X_val,stable_features,mrmr_features,b_k):
+    stable_cpg = list(stable_features.index) if hasattr(stable_features, "index") else list(stable_features)
+    mrmr_cpg = list(mrmr_features)
+    results = []
+
+    for method, feature in [("Stability Selection", stable_cpg),("mRMR", mrmr_cpg)]:
+        # preprocessing only on selected CpGs
+        preprocessor = preprocessor_pipeline(cpg=feature, metadata=[])
+
+        X_train_processed = preprocessor.fit_transform(X_train[feature])
+        X_val_processed = preprocessor.transform(X_val[feature])
+
+        y_train = X_train["age"].values
+        y_val = X_val["age"].values
+
+        # model of use BR
+        model, y_val_predict = bayesianridge_model(X_train_processed,y_train,X_val_processed)
+
+        rmse = mean_squared_error(y_val, y_val_predict) ** 0.5
+        r2 = r2_score(y_val, y_val_predict)
+        #######
+        results.append({
+            "Method": method,
+            "Number of Features": len(feature),
+            "Chosen K": b_k if method == "mRMR" else "-",
+            "Validation RMSE": rmse,
+            "Validation R^2": r2
+        })
+
+        print(f"\n{method})
+        print(f"Number of selected features : {len(feature)}")
+        if method == "mRMR":
+            print(f"Chosen K                    : {b_k}")
+        print(f"Validation RMSE             : {rmse:.4f}")
+        print(f"Validation R^2            : {r2:.4f}")
+
+    comparison_df = pd.DataFrame(results)
+
+    # selecting the best method based on RMSE (lower one)
+    best = comparison_df.loc[comparison_df["Validation RMSE"].idxmin()]
+    selected_method = best["Method"]
+    if selected_method == "Stability Selection":
+        selected_features = stable_cpg
+    else:
+        selected_features = mrmr_cpg
+
+    print("\n" + "=" * 60)
+    print("Feature selection summary-comparison")
+    print("=" * 60)
+    print(comparison_df.to_string(index=False))
+    print("=" * 60)
+    print(f"Selected method (later use): {selected_method}")
+    print(f"Number of features kept : {len(selected_features)}")
+    print(f"Best validation RMSE : {best['Validation RMSE']:.4f}")
+    print("=" * 60)
+
+    return comparison_df, selected_features, selected_method
 
 
 
